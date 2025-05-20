fc7d3ea35931b76a75a202de15d1c9323f1e7fce
MegaShpak322 <molka.dmitry93@gmail.com>
2025-05-20 20:08:09 +0300

    added changes after practice 2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+'use strict';
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,12 +10,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    let a = null,
+        b; 
+    while (a === null || a === '' || a.length > 50) {
+        a = prompt('Один из последних просмотренных фильмов?', '');
+    }
+    while (b === null || b === '' || isNaN(b)) {
+        b = prompt('На сколько оцените его?', '');
+    }
+    personalMovieDB.movies[a] = +b;
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count === null || personalMovieDB.count === '' || isNaN(personalMovieDB.count)) {
+    console.log("Произошла ошибка");
+} else if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Вы классический зритель");
+}
 
 console.log(personalMovieDB);
